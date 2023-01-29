a3085dfd271ba4b83c0dfb3dddfa742fe6f9a19e
Chiedozie Ikeanyi <ikeanyi.chiedozie@gmail.com>
2023-01-29 10:08:29 +0100

    create userRoutes.js

--- server.js
+++ server.js
@@ -7,9 +7,15 @@ const errorHandler = require("./middleware/errorHandler");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+const { logEvents } = require("./middleware/logger");
 const PORT = process.env.PORT || 3500;
 
 console.log(process.env.NODE_ENV);
+
+connectDB();
 
 app.use(logger);
 app.use(cors(corsOptions));
@@ -17,6 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);
@@ -31,4 +38,15 @@ app.all("*", (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
