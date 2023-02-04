941fd761614e60ed2dc50fd63fc6ac248231c76b
Chiedozie Ikeanyi <ikeanyi.chiedozie@gmail.com>
2023-02-04 08:28:56 +0100

    write getAllNotes function

--- controllers/notesController.js
+++ controllers/notesController.js
@@ -1,11 +1,32 @@
 const Note = require("../models/Note");
 const User = require("../models/User");
 const asyncHandler = require("express-async-handler");
+const { findById } = require("../models/User");
 
 // @desc Get all notes
 // @route GET /notes
 // @access Private
-const getAllNotes = asyncHandler(async (req, res) => {});
+const getAllNotes = asyncHandler(async (req, res) => {
+  // get all notes from mongodb
+  const notes = await Note.find().lean();
+
+  // if no notes
+  if (!notes?.length) {
+    return res.status(400).json({ message: "No notes found" });
+  }
+
+  // add username to each note before sending the response
+  // See Promise.all with map() here: https://youtu.be/4lqJBBEpjRE
+  // You could also do this with a for...of loop
+  const notesWithUser = await Promise.all(
+    notes.map(async (note) => {
+      const user = await User.findById(note.user).lean().exec();
+      return { ...note, username: user.username };
+    })
+  );
+
+  res.json(notesWithUser);
+});
 
 // @desc Create new note
 // @route POST /notes
